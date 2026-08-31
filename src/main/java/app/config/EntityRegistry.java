95dadb3007338de400ceebc091ef6dcd35728458
AndreasJensen1997 <planeten@live.dk>
2026-08-31 20:11:54 +0200

    Added all entities to EntityRegistry

--- src/main/java/app/config/EntityRegistry.java
+++ src/main/java/app/config/EntityRegistry.java
@@ -1,8 +1,8 @@
 package app.config;
 
-import app.entities.Chapter;
-import app.entities.AppUser;
+import app.entities.*;
 import org.hibernate.cfg.Configuration;
+
 
 final class EntityRegistry {
 
@@ -12,7 +12,16 @@ final class EntityRegistry {
     static void registerEntities(Configuration configuration) {
 
         configuration.addAnnotatedClass(AppUser.class);
+        configuration.addAnnotatedClass(Artifact.class);
         configuration.addAnnotatedClass(Chapter.class);
+        configuration.addAnnotatedClass(Fragment.class);
+        configuration.addAnnotatedClass(List.class);
+        configuration.addAnnotatedClass(Memory.class);
+        configuration.addAnnotatedClass(Mention.class);
+        configuration.addAnnotatedClass(Person.class);
+        configuration.addAnnotatedClass(Place.class);
+        configuration.addAnnotatedClass(Story.class);
+        configuration.addAnnotatedClass(TimeCapsule.class);
 
 
     }

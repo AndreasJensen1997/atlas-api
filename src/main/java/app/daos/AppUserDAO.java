90bb71b9ea5a669a599095dfe1caadb6ce1d9166
AndreasJensen1997 <planeten@live.dk>
2026-09-04 00:30:20 +0200

    Updated AppUserDAO methods to include proper try/catch to throw APIExceptions upon failure.

--- src/main/java/app/daos/AppUserDAO.java
+++ src/main/java/app/daos/AppUserDAO.java
@@ -2,10 +2,14 @@ package app.daos;
 
 
 import app.entities.AppUser;
+import app.exceptions.ApiException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceException;
 import lombok.AllArgsConstructor;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -15,58 +19,116 @@ public class AppUserDAO implements IDAO<AppUser, Integer> {
 
     @Override
     public AppUser create(AppUser appUser) {
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-        em.persist(appUser);
-        em.getTransaction().commit();
-        em.close();
+        if (appUser == null) {
+            throw new ApiException(400, "User is required");
+        }
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            try {
+                em.persist(appUser);
+                em.getTransaction().commit();
+            } catch (PersistenceException e) {
+                if (em.getTransaction().isActive()) {
+                    em.getTransaction().rollback();
+                }
+                throw new ApiException(500, "Create user failed: " + e.getMessage());
+            } catch (RuntimeException e) {
+                if (em.getTransaction().isActive()) {
+                    em.getTransaction().rollback();
+                }
+                throw e;
+            }
+        }
         return appUser;
     }
 
     @Override
     public AppUser getById(Integer id) {
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-        AppUser appUser = em.find(AppUser.class, id);
-        em.getTransaction().commit();
-        em.close();
-        return appUser;
+        if (id == null) {
+            throw new ApiException(400, "Study id is required");
+        }
+        try (EntityManager em = emf.createEntityManager()) {
+            try {
+                AppUser appUser = em.find(AppUser.class, id);
+                if (appUser != null) {
+                    return appUser;
+                }
+                throw new ApiException(404, "User not found");
+            } catch (PersistenceException e) {
+                throw new ApiException(500, "Get user failed: " + e.getMessage());
+            }
+        }
     }
 
     @Override
     public Set<AppUser> getAll() {
         EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-        Set<AppUser> allAppUsers = (Set<AppUser>) em.createQuery("SELECT u FROM AppUser u ", AppUser.class)
-                .getResultList();
-        em.getTransaction().commit();
-        em.close();
-
-        return allAppUsers;
+        try {
+            List<AppUser> allAppUsers = em.createQuery("SELECT u FROM AppUser u", AppUser.class)
+                    .getResultList();
+            return new HashSet<>(allAppUsers);
+        } finally {
+            em.close();
+        }
     }
 
     @Override
     public AppUser update(AppUser appUser) {
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-        em.merge(appUser);
-        em.getTransaction().commit();
-        em.close();
-
-        return appUser;
+        if (appUser == null || appUser.getUserId() == null) {
+            throw new ApiException(400, "User id is required");
+        }
+        AppUser updated;
+        try (EntityManager em = emf.createEntityManager()) {
+            em.getTransaction().begin();
+            try {
+                AppUser existing = em.find(AppUser.class, appUser.getUserId());
+                if (existing == null) {
+                    throw new ApiException(404, "User not found");
+                }
+                updated = em.merge(appUser);
+                em.getTransaction().commit();
+            } catch (PersistenceException e) {
+                if (em.getTransaction().isActive()) {
+                    em.getTransaction().rollback();
+                }
+                throw new ApiException(500, "Update user failed: " + e.getMessage());
+            } catch (RuntimeException e) {
+                if (em.getTransaction().isActive()) {
+                    em.getTransaction().rollback();
+                }
+                throw e;
+            }
+        }
+        return updated;
     }
 
-    @Override
-    public void delete(AppUser appUser) {
-        EntityManager em = emf.createEntityManager();
-
-        em.getTransaction().begin();
-        em.remove(appUser);
-        em.getTransaction().commit();
-        em.close();
+@Override
+public boolean delete(Integer id) {
+    if (id == null) {
+        throw new ApiException(400, "User id is required");
     }
+    try (EntityManager em = emf.createEntityManager()) {
+        em.getTransaction().begin();
+        try {
+            AppUser userToRemove = em.find(AppUser.class, id);
+            if (userToRemove != null) {
+                em.remove(userToRemove);
+            } else {
+                throw new ApiException(404, "User not found");
+            }
+            em.getTransaction().commit();
+        } catch (PersistenceException e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw new ApiException(500, "User study failed: " + e.getMessage());
+        } catch (RuntimeException e) {
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+            throw e;
+        }
+    }
+    return true;
+}
 }

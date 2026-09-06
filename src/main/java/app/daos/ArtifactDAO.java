0659bfb38bbf1c656d2ada0ae4ef43965639f5a3
AndreasJensen1997 <planeten@live.dk>
2026-09-06 16:02:51 +0200

    added getArtifactsByType() to ArtifactDAO

--- src/main/java/app/daos/ArtifactDAO.java
+++ src/main/java/app/daos/ArtifactDAO.java
@@ -2,13 +2,28 @@ package app.daos;
 
 import app.entities.AppUser;
 import app.entities.Artifact;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
 
 public class ArtifactDAO extends AbstractDAO<Artifact, Integer> {
 
 
     public ArtifactDAO(EntityManagerFactory emf) {
         super(emf, Artifact.class);
+    }
+
+
+    public List<Artifact> getArtifactsByType(int userId, int artifactTypeId) {
+        try (EntityManager em = emf.createEntityManager()) {
+            String jpql = "SELECT a FROM Artifact a WHERE a.appUser.userId = :userId AND a.artifactType.artifactTypeId = :typeId";
+            TypedQuery<Artifact> query = em.createQuery(jpql, Artifact.class);
+            query.setParameter("userId", userId);
+            query.setParameter("typeId", artifactTypeId);
+            return query.getResultList();
+        }
     }
 
 

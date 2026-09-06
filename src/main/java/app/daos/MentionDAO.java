558644aba751b30ec27c6600fda5388483bf8644
AndreasJensen1997 <planeten@live.dk>
2026-09-06 16:01:25 +0200

    added @ToString annotation to mention entity

--- src/main/java/app/daos/MentionDAO.java
+++ src/main/java/app/daos/MentionDAO.java
@@ -13,4 +13,9 @@ public class MentionDAO extends AbstractDAO<Mention, Integer> {
 
 
 
+
+
+
+
+
 }
--- src/main/java/app/entities/Mention.java
+++ src/main/java/app/entities/Mention.java
@@ -8,6 +8,7 @@ import lombok.*;
 @Getter
 @Setter
 @NoArgsConstructor
+@ToString
 @AllArgsConstructor
 @Builder
 public class Mention {

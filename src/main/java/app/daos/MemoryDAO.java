265a454662951f42ed2642abafc6fb9cdb60d51e
AndreasJensen1997 <planeten@live.dk>
2026-09-06 16:00:58 +0200

    added getAllMemoriesByUserId to memoryDAO

--- src/main/java/app/daos/MemoryDAO.java
+++ src/main/java/app/daos/MemoryDAO.java
@@ -1,12 +1,36 @@
 package app.daos;
 
+import app.entities.Chapter;
 import app.entities.Memory;
+import app.exceptions.ApiException;
+import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceException;
+import jakarta.persistence.TypedQuery;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class MemoryDAO extends AbstractDAO <Memory, Integer> {
 
     public MemoryDAO(EntityManagerFactory emf) {
         super(emf, Memory.class);
+    }
+
+
+    public Set<Memory> getAllMemoriesByUserId(int id) {
+        try (EntityManager em = emf.createEntityManager()) {
+            String jpql = "SELECT m FROM Memory m WHERE m.chapter.appUser.userId = :id";
+
+            TypedQuery<Memory> query = em.createQuery(jpql, Memory.class);
+            query.setParameter("id", id);
+
+            List<Memory> list = query.getResultList();
+            return new HashSet<>(list);
+        } catch (PersistenceException e) {
+            throw new ApiException(500, "Failed to retrieve memory list: " + e.getMessage());
+        }
     }
 
 

6b5b2ba92a1bad713b61c492f29675e4dd125af2
AndreasJensen1997 <planeten@live.dk>
2026-09-11 21:21:02 +0200

    added relation enum for Person entity

---
+++ src/main/java/app/enums/Relation.java
@@ -0,0 +1,81 @@
+package app.enums;
+
+public enum Relation {
+    // Immediate Family
+    MOTHER,
+    FATHER,
+    SON,
+    DAUGHTER,
+    BROTHER,
+    SISTER,
+    HUSBAND,
+    WIFE,
+
+    // Extended Family
+    GRANDFATHER,
+    GRANDMOTHER,
+    GRANDSON,
+    GRANDDAUGHTER,
+    GREAT_GRANDFATHER,
+    GREAT_GRANDMOTHER,
+    GREAT_GRANDSON,
+    GREAT_GRANDDAUGHTER,
+    UNCLE,
+    AUNT,
+    COUSIN,
+    NEPHEW,
+    NIECE,
+
+    // In-Laws & Blended Family
+    MOTHER_IN_LAW,
+    FATHER_IN_LAW,
+    BROTHER_IN_LAW,
+    SISTER_IN_LAW,
+    SON_IN_LAW,
+    DAUGHTER_IN_LAW,
+    STEPMOTHER,
+    STEPFATHER,
+    STEPSON,
+    STEPDAUGHTER,
+    STEPBROTHER,
+    STEPSISTER,
+    HALF_BROTHER,
+    HALF_SISTER,
+
+    // Social & Peer Relations
+    FRIEND,
+    BEST_FRIEND,
+    ACQUAINTANCE,
+    NEIGHBOR,
+    ROOMMATE,
+    CLASSMATE,
+    TEAMMATE,
+
+    // Professional & Mentorship
+    COLLEAGUE,
+    BOSS,
+    EMPLOYEE,
+    MENTOR,
+    MENTEE,
+    TEACHER,
+    STUDENT,
+    ADVISOR,
+    CLIENT,
+
+    // Romantic & Legal
+    PARTNER,
+    FIANCE,
+    FIANCEE,
+    EX_PARTNER,
+    EX_SPOUSE,
+    GUARDIAN,
+    WARD,
+    GODFATHER,
+    GODMOTHER,
+    GODCHILD,
+    ADOPTIVE_PARENT,
+    ADOPTIVE_CHILD,
+    FOSTER_PARENT,
+    FOSTER_CHILD,
+
+}
